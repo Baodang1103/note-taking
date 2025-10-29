b8967e3e9e50e81df9a06e4b6fce3b6d4fb99f76
BaoDang <baodang505@gmail.com>
2025-10-29 14:46:21 +0700

    chuan bi cac file da xong

---
+++ user-service/user-springboot/src/main/java/com/example/userspringboot/UserSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.userspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSpringbootApplication.class, args);
+	}
+
+}
---
+++ user-service/user-springboot/src/test/java/com/example/userspringboot/UserSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
